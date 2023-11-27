db7a2257055973c7748329d6d47319a8710c2bc8
pietrostraneo <straneo_p@yahoo.it>
2023-11-27 15:22:42 +0100

    Remake of the script with new method

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
-let list = ["Pomodoro", "Grana padano", "Pancetta", "Tritato di manzo", "Carote", "Latte", "Olio"]
+let list = ["Pomodoro", "Grana padano", "Pancetta", "Tritato di manzo", "Carote", "Latte", "Olio"];
 let shown_list = document.querySelector("ul");
 
+console.log(list);
 let i = 0;
 
+
+// METODO 1
+
+// while(i < list.length){
+//     shown_list.innerHTML += `<li>${list[i]}</li>`
+//     console.log(list[i])
+//     i++
+// }
+
+
+
+// METODO 2
+
 while(i < list.length){
-    shown_list.innerHTML = `<li>${list[i]}</li>`
-    console.log(list[i])
-    i++
+    const li = document.createElement("li");
+    li.innerText = list[i];
+    shown_list.appendChild(li); 
+    console.log(list[i]);
+    i++;
 }
 
